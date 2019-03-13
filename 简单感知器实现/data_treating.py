905a3d527cfba2f9c73c12ef921263bf22fae04f
kylin5207 <38106679+kylin5207@users.noreply.github.com>
2019-03-13 22:51:51 +0800

    改正类标y的值
    山鸢尾为-1，变色鸢尾为1

--- 简单感知器实现/data_treating.py
+++ 简单感知器实现/data_treating.py
@@ -15,7 +15,7 @@ df = pd.read_csv("iris-data.csv",header=None)
 #将标记存储到y中
 y = df.iloc[0:100, 4].values
 
-#标准化鸢尾花标记，将山鸢尾标准化为1，变色鸢尾标准化为-1。
+#标准化鸢尾花标记，将山鸢尾标准化为-1，变色鸢尾标准化为1。
 normal_y = np.where(y == 'Iris-setosa', 1, -1)
 
 #提取两个特征：萼片长度（第1列）和花瓣长度（第3列）
