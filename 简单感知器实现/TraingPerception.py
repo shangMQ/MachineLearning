f2578cba3ff84c84a34f4b93edf1ca48d3fd43e5
kylin5207 <38106679+kylin5207@users.noreply.github.com>
2019-03-13 23:10:14 +0800

    完善分类错误折线图

--- 简单感知器实现/TraingPerception.py
+++ 简单感知器实现/TraingPerception.py
@@ -7,18 +7,18 @@ Created on Wed Feb 27 15:48:04 2019
 """
 import matplotlib.pyplot as plt
 from Perception import Perception
-from data_treating import x, normal_y
+from data_treating import X, normal_y
 
+fig, ax = plt.subplots(nrows=1, ncols=1)
 #利用鸢尾花数据来训练感知器模型
 ppn = Perception(eta = 0.1, n_iter = 10)
-ppn.fit(x, normal_y)
+ppn.fit(X, normal_y)
 
 #绘制每次迭代的错误分类数量的折线图
-plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of misclassifications')
-plt.show()
-
+ax.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
+ax.set_xlabel('Epochs')
+ax.set_ylabel('Number of misclassifications')
+fig.show()
 
 
 
