cd75ea2c3217887124cf67c1bd2e0aee4315703a
kylin5207 <38106679+kylin5207@users.noreply.github.com>
2019-03-13 23:12:53 +0800

    将特征向量重命名为X

--- 简单感知器实现/data_treating.py
+++ 简单感知器实现/data_treating.py
@@ -19,11 +19,11 @@ y = df.iloc[0:100, 4].values
 normal_y = np.where(y == 'Iris-setosa', 1, -1)
 
 #提取两个特征：萼片长度（第1列）和花瓣长度（第3列）
-x = df.iloc[0:100, [0, 2]].values
+X = df.iloc[0:100, [0, 2]].values
 
 #绘制散点图
-plt.scatter(x[:50,0], x[:50,1], color = 'red', marker = 'o', label = 'Setosa')
-plt.scatter(x[50:100,0], x[50:100,1], color = 'blue', marker = 'x', label = 'versicolor')
+plt.scatter(X[:50,0], X[:50,1], color = 'red', marker = 'o', label = 'Setosa')
+plt.scatter(X[50:100,0], X[50:100,1], color = 'blue', marker = 'x', label = 'versicolor')
 #横轴是萼片长度，纵轴是花瓣长度。
 plt.xlabel('sepal length')
 plt.ylabel('petal length')
