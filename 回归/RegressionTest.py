72050f45ab88a0f5956cb14c33fa6a23be12532b
kylin5207 <38106679+kylin5207@users.noreply.github.com>
2019-05-22 22:24:32 +0800

    upload a easy test about calculating the square of a number
    利用梯度下降计算平方根

---
+++ 回归/RegressionTest.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 19:53:20 2019
+利用梯度下降法计算平方根
+@author: Kylin
+"""
+import math
+
+if __name__ == "__main__":
+    learning_rate = 0.01
+    for a in range(1,100):
+        cur = 0
+        for i in range(1000):
+            #令f(x)=0的最小的cur值，就是我们预测的平方根
+            cur -= learning_rate*(cur**2 - a)
+        print("{:}的平方根的真实值是{:.4f},预测值为{:.4f}".format(a, math.sqrt(a), cur))
+    
+
