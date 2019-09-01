1a83d8e62ed7e246e948ef8b49d67aa8d13d1008
kylin5207 <38106679+kylin5207@users.noreply.github.com>
2019-09-01 17:46:14 +0800

    前六个特征包含了数据的绝大多数信息，可以将原本的590维数据降为6维

--- 无监督学习/PCA/SemiConductor/pca.py
+++ 无监督学习/PCA/SemiConductor/pca.py
@@ -5,6 +5,7 @@ Created on Sun Sep  1 15:55:22 2019
 @author: Kylin
 """
 import numpy as np
+import numpy.linalg as la
 
 def loadData(filename, delim="\t"):
     """
@@ -32,9 +33,59 @@ def loadData(filename, delim="\t"):
 def replaceNanWithMean():
     """
     利用均值替换为NAN的数据
+    返回值：
+        填充后的数据
     """
     dataMat = loadData("secom.data", " ")
     FeatureNum = dataMat.shape[1]
-    print(FeatureNum)
     
+    for i in range(FeatureNum):
+        #均值(数组(非零(非nan(第i个特征数组))))
+        meanVal = np.mean(dataMat[np.nonzero(~np.isnan(dataMat[:,i].A))[0],i])
+        
+        #利用均值填充
+        dataMat[np.nonzero(np.isnan(dataMat[:,i].A))[0],i] = meanVal
+    
+    return dataMat
 
+def pca(dataMat, topNfeat):
+    """
+    PCA主函数
+    参数：
+        dataMat:数据矩阵
+        topNfeat:应用的N个特征，默认为1
+    返回值：
+        lowDDataMat:降维之后的数据集
+        reConMat:重构数据集
+    """
+    #1. 减去平均值
+    meanVals = np.mean(dataMat, axis = 0)
+    meanRemoved = dataMat - meanVals
+
+    #2. 计算协方差矩阵
+    covMat = np.mat(np.cov(meanRemoved, rowvar = 0))
+    
+    #3. 计算协方差矩阵的特征值和特征向量
+    eigVals, eigVects = la.eig(covMat)
+    
+    #4. 将特征值从大到小排序, 保留下标，保留最前面的topNfeat个特征值
+    eigValInd = np.argsort(eigVals)
+    eigValInd = eigValInd[:-(topNfeat+1):-1]
+
+    #5. 保留topNfeat个特征向量
+    redEigVects = eigVects[:, eigValInd]
+    print(redEigVects)
+    
+    #6. 将数据转换到新的空间中
+    lowDDataMat = meanRemoved * redEigVects
+    
+    #7. 重构数据集
+    reConMat = (lowDDataMat * redEigVects.T) + meanVals
+    
+    return lowDDataMat, reConMat
+    
+if __name__ == "__main__":
+    dataMat = replaceNanWithMean()
+    MaxFeatures = 6
+    lowDDataMat, reConMat = pca(dataMat, MaxFeatures)
+    print(lowDDataMat)
