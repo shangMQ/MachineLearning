a0b86f8d32199492eaf7a3fcdb6383c7643cef70
Kylin <38106679+kylin5207@users.noreply.github.com>
2021-02-07 20:20:29 +0800

    单变量特征选择再cancer数据集上的应用，发现可以提高模型的性能

--- 数据预处理/特征选择/autoFeatureSelection/ANOVA.py
+++ 数据预处理/特征选择/autoFeatureSelection/ANOVA.py
@@ -11,47 +11,53 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
 
-#1. 加载数据集
+# 1. 加载数据集
 cancer = load_breast_cancer()
 X, y = cancer.data, cancer.target
 print("initial data shape:", X.shape)
 
-#2. 向原数据集中添加噪声
+# 2. 向原数据集中添加噪声
 rng = np.random.RandomState(42)
-noise = rng.normal(size = (len(X), 50))
+noise = rng.normal(size=(len(X), 50))
 X_withNoise = np.hstack([X, noise])
-#含噪声特征的数据集中前30个特征来自数据集，后50个是我们添加的噪声特征
+# 含噪声特征的数据集中前30个特征来自数据集，后50个是我们添加的噪声特征
 print("add noise data shape:", X_withNoise.shape)
 
-#3. 划分训练集与测试集
+# 3. 划分训练集与测试集
 X_train, X_test, y_train, y_test = train_test_split(X_withNoise, y, test_size=0.5, random_state=0)
 
-#4. 使用f_classif和SelectPercentile选择50%的特征
+# 4. 使用f_classif和SelectPercentile选择50%的特征
 select = SelectPercentile(percentile=50)
 select.fit(X_train, y_train)
-#对训练集进行变换
-#X_train_selected = select.transform(X_train)#这里会有一个异常提示出现
-#print("After feature selection data shape:", X_train_selected.shape)
-#
-#5. 查看哪些特征被选中
-#mask = select.get_support()
-#print("被选中的特征：", mask)
-##蓝色的表示选中True，白色的表示False即未选中的特征
-#plt.matshow(mask.reshape(1,-1), cmap="GnBu")
-#plt.xlabel("Sample index")
 
-#7. 利用原始数据的全部特征查看拟合效果。
+# 对训练集进行变换
+X_train_selected = select.transform(X_train)  # 这里会有一个异常提示出现
+print("After feature selection data shape:", X_train_selected.shape)
+
+# 5. 查看哪些特征被选中
+mask = select.get_support()
+print("被选中的特征：", mask)
+# 蓝色的表示选中True，白色的表示False即未选中的特征
+plt.matshow(mask.reshape(1, -1), cmap="gray_r")
+plt.xlabel("Sample index")
+plt.show()
+
+# 7. 利用原始数据的全部特征查看拟合效果。
 logisticReg = LogisticRegression()
 logisticReg.fit(X_train, y_train)
-print("Initial Data Score:", logisticReg.score(X_test, y_test))
+print("=======Initial Data==========")
+print("Train Score:", logisticReg.score(X_train, y_train))
+print("Test Score:", logisticReg.score(X_test, y_test))
 
-#8. 利用原始数据选择80%的特征查看拟合效果。
-select = SelectPercentile(percentile=80)
+# 8. 利用原始数据选择80%的特征查看拟合效果。
+select = SelectPercentile(percentile=50)
 select.fit(X_train, y_train)
 X_train_sixty = select.transform(X_train)
 X_test_sixty = select.transform(X_test)
 logisticReg.fit(X_train_sixty, y_train)
-print("Select 80% features Data Score:", logisticReg.score(X_test_sixty, y_test))
-
+print("======60 % Feature Selection Data==========")
+print("Train Score:", logisticReg.score(X_train_sixty, y_train))
+print("Test Score:", logisticReg.score(X_test_sixty, y_test))
 
