bce00df13d466362b85091f278c69dec5a53ecc9
Kylin <38106679+kylin5207@users.noreply.github.com>
2021-10-28 14:17:09 +0800

    update: modify the drawing method

--- 简单感知器实现/TraingPerception.py
+++ 简单感知器实现/TraingPerception.py
@@ -10,15 +10,17 @@ from Perception import Perception
 from data_treating import X, normal_y
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
-#利用鸢尾花数据来训练感知器模型
+# 利用鸢尾花数据来训练感知器模型
 ppn = Perception(eta = 0.1, n_iter = 10)
 ppn.fit(X, normal_y)
 
-#绘制每次迭代的错误分类数量的折线图
-ax.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
-ax.set_xlabel('Epochs')
-ax.set_ylabel('Number of misclassifications')
-fig.show()
+# 绘制每次迭代的错误分类数量的折线图
+plt.plot(range(1, len(ppn.errors_)+1), ppn.errors_, marker='o')
+# 横轴为迭代次数
+plt.xlabel('Epochs')
+# 纵轴为分类错误的次数
+plt.ylabel('Number of misclassifications')
+plt.show()
 
 
 
