5bd17639a253e3a020ef38a61a3ef305ef96c8a7
Kylin <38106679+kylin5207@users.noreply.github.com>
2022-06-01 19:30:34 +0800

    docs: add more notification

--- 期望最大化/EM_intro.py
+++ 期望最大化/EM_intro.py
@@ -11,61 +11,66 @@ from sklearn.mixture import GaussianMixture
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sklearn.metrics.pairwise import pairwise_distances_argmin, pairwise_distances
 
 
-def main():
-    # 相关设置
-    mpl.rcParams['font.sans-serif'] = 'Arial Unicode MS'
-    mpl.rcParams['axes.unicode_minus'] = False
+mpl.rcParams['font.sans-serif'] = [u'SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
 
+
+if __name__ == '__main__':
     style = 'sklearn'
+    # style = 'hand'
 
-    # 1. 加载数据
     np.random.seed(0)
-
-    # 第一组高斯数据（均值，方差）
+    # 数据集1
+    # 多维分布的均值
     mu1_fact = (0, 0, 0)
-    cov1_fact = np.diag((1, 2, 3))
-    data1 = np.random.multivariate_normal(mu1_fact, cov1_fact, 400)
+    # 创建一个3*3的标准矩阵作为协方差矩阵
+    cov_fact = np.identity(3)
+    data1 = np.random.multivariate_normal(mu1_fact, cov_fact, 400)
 
-    # 第二组高斯数据（均值，方差）
-    mu2_fact = (4, 2, 1)
-    cov2_fact = np.array(((1, 1, 3), (1, 2, 1), (0, 0, 1)))
-    data2 = np.random.multivariate_normal(mu2_fact, cov2_fact, 100)
+    # 数据集2
+    # 多维分布的均值
+    mu2_fact = (2, 2, 1)
+    # 创建一个3*3的标准矩阵作为协方差矩阵
+    cov_fact = np.identity(3)
+    # 生成一个多元正态分布
+    data2 = np.random.multivariate_normal(mu2_fact, cov_fact, 100)
 
+    # 合并数据集，构造一个混合数据集
     data = np.vstack((data1, data2))
-    print("data shape:", data.shape)
-
-    # 定义标签，为了便于计算正确率
+    # 其中前400个样本属于第一个数据集，后100个样本属于第二个数据集
     y = np.array([True] * 400 + [False] * 100)
 
-    # 2. 利用EM计算高斯模型的相关参数
     if style == 'sklearn':
-        # 使用sklearn提供的EM模型进行拟合计算
+        # n_components设定混合高斯模型的个数
+        # covariance_type设定协方差类型
+        # tol 阈值
+        # max_iter 最大迭代次数
+        # n_init 初始化次数，用于产生最佳初始参数
         g = GaussianMixture(n_components=2, covariance_type='full', tol=1e-6, max_iter=1000)
         g.fit(data)
-        print('类别概率:\t', g.weights_[0])
-        print('均值:\n', g.means_, '\n')
-        print('方差:\n', g.covariances_, '\n')
+        print('类别概率:\t', g.weights_)
+        print('混合模型的均值:\n', g.means_, '\n')
+        print('混合模型的方差:\n', g.covariances_, '\n')
         mu1, mu2 = g.means_
         sigma1, sigma2 = g.covariances_
     else:
-        # 自实现EM算法
         num_iter = 100
         n, d = data.shape
-
         # 随机指定
         # mu1 = np.random.standard_normal(d)
+        # print mu1
         # mu2 = np.random.standard_normal(d)
-
-        # 2.1 给定theta参数的相关初始化值
+        # print mu2
+        # 随机初始化均值，一个选择最小值，一个选择最大值
         mu1 = data.min(axis=0)
         mu2 = data.max(axis=0)
-        # 创建单位对角方阵
+        # 随机初始化方差
         sigma1 = np.identity(d)
         sigma2 = np.identity(d)
-
+        # 随机初始化高斯分布权重
         pi = 0.5
 
         # EM
@@ -88,23 +93,24 @@ def main():
         print('均值:\t', mu1, mu2)
         print('方差:\n', sigma1, '\n\n', sigma2, '\n')
 
-    # 3. 预测分类
+    # 预测分类
+    # 根据最终拟合的模型构建高斯模型
     norm1 = multivariate_normal(mu1, sigma1)
     norm2 = multivariate_normal(mu2, sigma2)
+    # 根据用户数据，计算相关的高斯值
     tau1 = norm1.pdf(data)
     tau2 = norm2.pdf(data)
-
-    fig = plt.figure(figsize=(10, 5), facecolor='w')
-    # 原始数据
+    # 数据可视化
+    fig = plt.figure(figsize=(13, 7), facecolor='w')
     ax = fig.add_subplot(121, projection='3d')
-    ax.scatter(data[:, 0], data[:, 1], data[:, 2], c='b', s=30, marker='o', edgecolors='k', depthshade=True)
+    ax.scatter(data[:, 0], data[:, 1], data[:, 2], c='b', s=30, marker='o', depthshade=True)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_title('原始数据', fontsize=15)
-
-    # 利用EM计算得到的相关高斯分布参数生成的数据
+    ax.set_title(u'原始数据', fontsize=18)
     ax = fig.add_subplot(122, projection='3d')
+    # 计算计算值与真实值的误差
+    print(pairwise_distances([mu1_fact, mu2_fact], [mu1, mu2], metric='euclidean'))
     order = pairwise_distances_argmin([mu1_fact, mu2_fact], [mu1, mu2], metric='euclidean')
     print(order)
     if order[0] == 0:
@@ -113,19 +119,14 @@ def main():
         c1 = tau1 < tau2
     c2 = ~c1
     acc = np.mean(y == c1)
-
-    print('准确率：%.2f%%' % (100 * acc))
-    ax.scatter(data[c1, 0], data[c1, 1], data[c1, 2], c='r', s=30, marker='o', edgecolors='k', depthshade=True)
-    ax.scatter(data[c2, 0], data[c2, 1], data[c2, 2], c='g', s=30, marker='^', edgecolors='k', depthshade=True)
+    print(u'准确率：%.2f%%' % (100*acc))
+    ax.scatter(data[c1, 0], data[c1, 1], data[c1, 2], c='r', s=30, marker='o', depthshade=True)
+    ax.scatter(data[c2, 0], data[c2, 1], data[c2, 2], c='g', s=30, marker='^', depthshade=True)
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_title('EM算法分类', fontsize=15)
-    plt.suptitle('EM算法的实现', fontsize=18)
-    plt.subplots_adjust(top=0.90)
+    ax.set_title(u'EM算法分类', fontsize=18)
+    # plt.suptitle(u'EM算法的实现', fontsize=20)
+    # plt.subplots_adjust(top=0.92)
     plt.tight_layout()
     plt.show()
-
-
-if __name__ == '__main__':
-    main()
